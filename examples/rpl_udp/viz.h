f1ea68783c6046cb04ff7df648a9a081641fd9c6
Cedric Adjih <cedric.adjih@inria.fr>
2014-11-11 23:07:50 +0100

    put definitions in viz.h

---
+++ examples/rpl_udp/viz.h
@@ -0,0 +1,12 @@
+/* CA: prototypes for viz.c */
+
+void viz_udp_pkt(uint8_t src);
+
+void viz_udp_snd(uint8_t dst);
+
+void viz_parent_select(uint8_t parent);
+
+void viz_parent_deselect(uint8_t parent);
+
+void viz_dio(uint8_t src);
+
